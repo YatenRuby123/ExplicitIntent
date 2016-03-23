72c2d57af08d2bf1b500b2db1b63a4aa90641cac
Yatendra Pawar <rubyyatpaw2100@gmail.com>
2016-03-22 17:30:06 -0700

    Initial commit

---
+++ app/src/main/java/com/explicitintent/yatpawjava/explicitintent/ActivityB.java
@@ -0,0 +1,20 @@
+package com.explicitintent.yatpawjava.explicitintent;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Yat-dee on 3/17/2016.
+ */
+public class ActivityB extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_b);
+
+
+    }
+}
